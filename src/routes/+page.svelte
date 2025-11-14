a49965644c68511ac6ebda6de0a5632ad7725c45
ElvisCtf <elvischengtf@gmail.com>
2025-11-14 11:40:56 +0800

    fix: Missing handling after picking PDFs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,6 +51,7 @@
         type="file" 
         accept="application/pdf" 
         multiple
+        on:change={handleFiles}
         class="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 
               file:rounded-full file:border-0 file:text-sm file:font-semibold 
               file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
